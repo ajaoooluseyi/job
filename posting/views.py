c7e8db3306b8717de3bf2040957c52c88b075d6c
demex1 <ajaoooluseyi@gmail.com>
2023-07-30 15:38:59 +0100

    commit

--- posting/views.py
+++ posting/views.py
@@ -6,3 +6,14 @@ from .models import JobPosition
 def get_job_positions(request):
     job_positions = JobPosition.objects.all().values()
     return JsonResponse({"job_positions": list(job_positions)})
+
+
+def search_job_positions(request):
+    if request.method == "GET":
+        selected_job = request.GET.get("selected_job", "")
+        job_positions = JobPosition.objects.filter(
+            title__icontains=selected_job
+        ).values()
+        return JsonResponse({"job_positions": list(job_positions)})
+    else:
+        return JsonResponse({"error": "Invalid request method."}, status=400)
